c02f1a3468a49b4eccb687534995787a77db5a57
SachdevaVansh <bh2ggsipu@gmail.com>
2026-03-22 23:39:45 +0530

    Added some comments [skip ci]

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './index.css';
 
 // Making the change to test the workflow. This is an important line. It tells React to render the App component inside the element with the id of 'root' in the HTML file. This is where our React application will be displayed on the webpage.
 //Change 2
+// Skip the workflow
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
