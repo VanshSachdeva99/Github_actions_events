f4392f3cc02f2f3b845e1172ebd414d6a7868305
SachdevaVansh <bh2ggsipu@gmail.com>
2026-03-22 23:30:05 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 // Making the change to test the workflow. This is an important line. It tells React to render the App component inside the element with the id of 'root' in the HTML file. This is where our React application will be displayed on the webpage.
-
+//Change 2
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
